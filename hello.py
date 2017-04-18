fcbf5ab3b15826bcfad5707c3d958352d189fd45
adamolechno <adamolechno@gmail.com>
2017-04-18 15:10:02 -0400

    Add line that list files in current direacoty in os

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
-#!/usb/bin/env python
+#!/usr/bin/env python
+import sys 
+import subprocess
 
 print ('Hello world')
 print ('Another hello to tests git pull command')
+
+subprocess.check_output(["echo", "Hello World!"])
+subprocess.check_call(["ls", "-l"])
